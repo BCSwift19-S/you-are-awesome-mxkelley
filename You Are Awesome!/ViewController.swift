df72d381a910d20dc5daee4b18df85899ad4489b
Mr. Michael Kelley <mx@kell4.com>
2019-01-07 19:08:51 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Michael X Kelley on 1/7/19.
+//  Copyright © 2019 Michael X Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
